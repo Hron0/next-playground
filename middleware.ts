81c1691b342bbcc2a9781ab76173c360b4bf5ab0
Hron0 <sasha1254370@gmail.com>
2024-04-22 15:15:20 +0300

    asdasdadawdsdadasdsaas

--- middleware.ts
+++ middleware.ts
@@ -38,3 +38,5 @@ export default NextAuth(authConfig).auth((req) => {
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 }
+
+export const runtime = "experimental-edge"
